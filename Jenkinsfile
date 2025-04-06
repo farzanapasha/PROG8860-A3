2b8a90fbbfe3563038f3ff5346e0a4482f6bf2c3
Farzana Pasha Jahangeer <farzanapasha.farzana@gmail.com>
2025-04-05 21:24:10 -0400

    added test

--- Jenkinsfile
+++ Jenkinsfile
@@ -31,28 +31,23 @@ pipeline {
             }
         }
 
+        stage('Run Unit Tests') {
+            steps {
+                sh 'npm test'
+            }
+        }
+
 	stage('Deploy to Azure') {
     	    steps {
                 withCredentials([azureServicePrincipal('e649048f-b49b-4d7e-93b5-7cab92e8df4d')]) {
                     sh '''
-			echo "CLIENT_ID: $AZURE_CLIENT_ID"
-	                echo "CLIENT_SECRET: $AZURE_CLIENT_SECRET"
-        	        echo "TENANT_ID: $AZURE_TENANT_ID"
-                	echo "SUBSCRIPTION_ID: $AZURE_SUBSCRIPTION_ID"
- 			echo "$AZURE_FUNCTIONAPP_NAME: $AZURE_FUNCTIONAPP_NAME"
-
                         az login --service-principal \
                           -u $AZURE_CLIENT_ID \
                           -p $AZURE_CLIENT_SECRET \
                           --tenant $AZURE_TENANT_ID
 
 			az account set --subscription $AZURE_SUBSCRIPTION_ID
-
 			az account show
-			
-			echo "Directory: $(pwd)"
-			ls -lh
-
 			func azure functionapp publish $AZURE_FUNCTIONAPP_NAME
                     '''
                 }
--- src/functions/HelloWorld.js
+++ src/functions/HelloWorld.js
@@ -1,13 +1,34 @@
 const { app } = require('@azure/functions');
 
+const helloWorldHandler = async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.name || await request.text() || 'world'; // directly access request.query.name
+
+    return { body: `Hello, ${name}!` };
+};
+
+// Register the handler with Azure Functions
 app.http('HelloWorld', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+    handler: helloWorldHandler
 });
+
+// Export the handler for testing
+module.exports = helloWorldHandler;
+
+
+//const { app } = require('@azure/functions');
+//
+//app.http('HelloWorld', {
+//    methods: ['GET', 'POST'],
+//    authLevel: 'anonymous',
+//    handler: async (request, context) => {
+//        context.log(`Http function processed request for url "${request.url}"`);
+//
+//        const name = request.query.get('name') || await request.text() || 'world';
+//
+//        return { body: `Hello, ${name}!` };
+//    }
+//});
